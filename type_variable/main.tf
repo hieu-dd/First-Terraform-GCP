bda357e2a6b432a912d62ec1b48b21f6324db9f9
14H <doduchieu.kstn@gmail.com>
2022-04-20 17:22:12 +0700

    Update multiple provider

---
+++ type_variable/main.tf
@@ -0,0 +1,4 @@
+resource local_file sample_res {
+    filename = var.file_name
+    content = var.content[1]
+}
---
+++ type_variable/variable.tf
@@ -0,0 +1,9 @@
+variable file_name {
+    type = string
+    default = "sample.txt"
+}
+
+variable content {
+  type = list(string)
+  default = ["12","34","56"]
+}
